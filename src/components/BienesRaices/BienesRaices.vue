6fcf337987fc4c87aaa92affc8aee54d48924187
Isardbp <isardbp@gmail.com>
2019-02-19 12:37:52 +0100

    changed NuestrosProyectos & add Bienes Raices

--- src/components/BienesRaices/BienesRaices.vue
+++ src/components/BienesRaices/BienesRaices.vue
@@ -1,3 +1,183 @@
 <template>
-    <h1>Bienes Raices</h1>
+    <v-container grid-list-lg >
+        <v-layout row>
+            <v-flex xs12 class="text-xs-center display-1 font-weight-black my-5">Bienes Raices</v-flex>
+        </v-layout>
+        <v-layout row wrap>
+            <v-flex xs12 sm12 md4>
+                <v-card >
+                    <v-card-title><h2>House Name</h2></v-card-title>
+                    <v-responsive>
+                        <v-img src="https://upload.wikimedia.org/wikipedia/commons/1/15/Revised_Baca_House%2C_Trinidad%2C_CO_IMG_5081.JPG" height="300px">
+                        </v-img>
+                    </v-responsive>
+                    <v-card-text>
+                        <div>
+                            <v-layout>
+                                <v-flex xs6><h4>1234€</h4></v-flex>
+                                <v-flex xs6><h4 class="text-xs-right">1112€</h4></v-flex>
+                            </v-layout>
+                                <v-progress-linear v-model="valueDeterminate"></v-progress-linear>
+                            <v-layout>
+                                <v-flex xs6><h4>13 inversores</h4></v-flex>
+                                <v-flex xs6><h4 class="text-xs-right">40 dias restantes</h4></v-flex>
+                            </v-layout>
+                            <v-spacer></v-spacer>
+                        </div>
+                        <v-divider></v-divider>
+                        <div>
+                            <v-spacer></v-spacer>
+                            <v-layout align-center justify-space-around row fill-height>
+                                <v-flex xs6 class="text-md-center">
+                                    <h2>4.50%</h2>
+                                    <p>Anual</p>
+                                </v-flex>
+                                <v-divider vertical></v-divider>
+                                <v-flex xs6 class="text-md-center">
+                                    <h2>22.50%</h2>
+                                    <p>Total</p>
+                                </v-flex>
+                            </v-layout>
+                        </div>
+                    </v-card-text>
+                    <v-card-actions>
+                        <v-btn outline block color="green" @click="showRecipes('keto')">Select This Plan</v-btn>
+                    </v-card-actions>
+                </v-card>
+            </v-flex>
+            <v-flex xs12 sm12 md4>
+                <v-card >
+                    <v-card-title><h2>House Name</h2></v-card-title>
+                    <v-responsive>
+                        <v-img src="https://upload.wikimedia.org/wikipedia/commons/1/15/Revised_Baca_House%2C_Trinidad%2C_CO_IMG_5081.JPG" height="300px">
+                        </v-img>
+                    </v-responsive>
+                    <v-card-text>
+                        <div>
+                            <v-layout>
+                                <v-flex xs6><h4>1234€</h4></v-flex>
+                                <v-flex xs6><h4 class="text-xs-right">1112€</h4></v-flex>
+                            </v-layout>
+                                <v-progress-linear v-model="valueDeterminate"></v-progress-linear>
+                            <v-layout>
+                                <v-flex xs6><h4>13 inversores</h4></v-flex>
+                                <v-flex xs6><h4 class="text-xs-right">40 dias restantes</h4></v-flex>
+                            </v-layout>
+                            <v-spacer></v-spacer>
+                        </div>
+                        <v-divider></v-divider>
+                        <div>
+                            <v-spacer></v-spacer>
+                            <v-layout align-center justify-space-around row fill-height>
+                                <v-flex xs6 class="text-md-center">
+                                    <h2>4.50%</h2>
+                                    <p>Anual</p>
+                                </v-flex>
+                                <v-divider vertical></v-divider>
+                                <v-flex xs6 class="text-md-center">
+                                    <h2>22.50%</h2>
+                                    <p>Total</p>
+                                </v-flex>
+                            </v-layout>
+                        </div>
+                    </v-card-text>
+                    <v-card-actions>
+                        <v-btn outline block color="green" @click="showRecipes('keto')">Select This Plan</v-btn>
+                    </v-card-actions>
+                </v-card>
+            </v-flex>
+            <v-flex xs12 sm12 md4>
+                <v-card >
+                    <v-card-title><h2>House Name</h2></v-card-title>
+                    <v-responsive>
+                        <v-img src="https://upload.wikimedia.org/wikipedia/commons/1/15/Revised_Baca_House%2C_Trinidad%2C_CO_IMG_5081.JPG" height="300px">
+                        </v-img>
+                    </v-responsive>
+                    <v-card-text>
+                        <div>
+                            <v-layout>
+                                <v-flex xs6><h4>1234€</h4></v-flex>
+                                <v-flex xs6><h4 class="text-xs-right">1112€</h4></v-flex>
+                            </v-layout>
+                                <v-progress-linear v-model="valueDeterminate"></v-progress-linear>
+                            <v-layout>
+                                <v-flex xs6><h4>13 inversores</h4></v-flex>
+                                <v-flex xs6><h4 class="text-xs-right">40 dias restantes</h4></v-flex>
+                            </v-layout>
+                            <v-spacer></v-spacer>
+                        </div>
+                        <v-divider></v-divider>
+                        <div>
+                            <v-spacer></v-spacer>
+                            <v-layout align-center justify-space-around row fill-height>
+                                <v-flex xs6 class="text-md-center">
+                                    <h2>4.50%</h2>
+                                    <p>Anual</p>
+                                </v-flex>
+                                <v-divider vertical></v-divider>
+                                <v-flex xs6 class="text-md-center">
+                                    <h2>22.50%</h2>
+                                    <p>Total</p>
+                                </v-flex>
+                            </v-layout>
+                        </div>
+                    </v-card-text>
+                    <v-card-actions>
+                        <v-btn outline block color="green" @click="showRecipes('keto')">Select This Plan</v-btn>
+                    </v-card-actions>
+                </v-card>
+            </v-flex>
+            <v-flex xs12 sm12 md4>
+                <v-card >
+                    <v-card-title><h2>House Name</h2></v-card-title>
+                    <v-responsive>
+                        <v-img src="https://upload.wikimedia.org/wikipedia/commons/1/15/Revised_Baca_House%2C_Trinidad%2C_CO_IMG_5081.JPG" height="300px">
+                        </v-img>
+                    </v-responsive>
+                    <v-card-text>
+                        <div>
+                            <v-layout>
+                                <v-flex xs6><h4>1234€</h4></v-flex>
+                                <v-flex xs6><h4 class="text-xs-right">1112€</h4></v-flex>
+                            </v-layout>
+                                <v-progress-linear v-model="valueDeterminate"></v-progress-linear>
+                            <v-layout>
+                                <v-flex xs6><h4>13 inversores</h4></v-flex>
+                                <v-flex xs6><h4 class="text-xs-right">40 dias restantes</h4></v-flex>
+                            </v-layout>
+                            <v-spacer></v-spacer>
+                        </div>
+                        <v-divider></v-divider>
+                        <div>
+                            <v-spacer></v-spacer>
+                            <v-layout align-center justify-space-around row fill-height>
+                                <v-flex xs6 class="text-md-center">
+                                    <h2>4.50%</h2>
+                                    <p>Anual</p>
+                                </v-flex>
+                                <v-divider vertical></v-divider>
+                                <v-flex xs6 class="text-md-center">
+                                    <h2>22.50%</h2>
+                                    <p>Total</p>
+                                </v-flex>
+                            </v-layout>
+                        </div>
+                    </v-card-text>
+                    <v-card-actions>
+                        <v-btn outline block color="green" @click="showRecipes('keto')">Select This Plan</v-btn>
+                    </v-card-actions>
+                </v-card>
+            </v-flex>
+        </v-layout>
+    </v-container>
 </template>
+
+<script>
+export default {
+    data () {
+        return {
+            valueDeterminate: 50
+        }
+    }  
+}
+</script>
--- src/components/NuestrosProyectos/NuestrosProyectos.vue
+++ src/components/NuestrosProyectos/NuestrosProyectos.vue
@@ -1,25 +1,38 @@
 <template>
-    <v-container grid-list-md>
+    <v-container grid-list-lg >
+        <v-layout row>
+            <v-flex xs12 class="text-xs-center display-1 font-weight-black my-5">Bienes Raices</v-flex>
+        </v-layout>
         <v-layout row wrap>
-            <v-flex
-            xs4
-            v-for="item in cards"
-            :key="item.title"
-            >
-                <v-card height="350" width="350">
-                    <v-img
-                    :src="item.img"
-                    aspect-ratio="2.75"
-                    ></v-img>
-                    <v-card-title primary-title>
+            <v-flex xs12 sm12 md4>
+                <v-card >
+                    <v-card-title><h2>Project Name</h2></v-card-title>
+                    <v-responsive>
+                        <v-img src="http://source.unsplash.com/hjCA3ecCXAQ" height="300px">
+                        </v-img>
+                    </v-responsive>
+                    <v-card-text>
                         <div>
-                            <h3 class="headline mb-0">{{item.title}}</h3>
-                        <div>{{item.info}}</div>
+                            <v-layout>
+                                <v-flex xs6><h4>135</h4></v-flex>
+                                <v-flex xs6><h4 class="text-xs-right">135</h4></v-flex>
+                            </v-layout>
+                                <v-progress-linear v-model="valueDeterminate"></v-progress-linear>
+                            <v-layout>
+                                <v-flex xs6><h4>135</h4></v-flex>
+                                <v-flex xs6><h4 class="text-xs-right">135</h4></v-flex>
+                            </v-layout>
+                            
                         </div>
-                    </v-card-title>
+                        <v-divider></v-divider>
+                        <div>
+                            <h3 class="headline mb-0">Keto</h3>
+                            <div>The Keto diet is a high-fat, adequate-protein, low-carbohydrate diet. The diet forces the body to burn fats rather than carbohydrates by putting the body into ketosis.
+                            </div>
+                        </div>
+                    </v-card-text>
                     <v-card-actions>
-                        <v-btn flat color="orange">Share</v-btn>
-                        <v-btn flat color="orange">Explore</v-btn>
+                        <v-btn outline block color="green" @click="showRecipes('keto')">Select This Plan</v-btn>
                     </v-card-actions>
                 </v-card>
             </v-flex>
@@ -36,7 +49,8 @@ export default {
                 {title: 'Proyecto 2', info: 'Esta es info del proyecto2 que bla bla bla bla', img: 'https://cdn.vuetifyjs.com/images/parallax/material2.jpg'},
                 {title: 'Proyecto 3', info: 'Esta es info del proyecto3 que bla bla bla bla', img: 'https://cdn.vuetifyjs.com/images/parallax/material.jpg'},
                 {title: 'Proyecto 4', info: 'Esta es info del proyecto4 que bla bla bla bla', img: 'https://picsum.photos/510/300?random'}
-            ]
+            ],
+            valueDeterminate: 50
         }
     }  
 }
